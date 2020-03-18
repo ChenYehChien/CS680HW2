fec9506ff1629479fe4e945165936102de5dba02
ChenYeh.Chien <ChenYeh.Chien001@umb.edu>
2020-03-17 20:08:21 -0400

    cs680HW2

---
+++ test/src/edu/umb/cs680/hw05/CarTest.java
@@ -0,0 +1,31 @@
+package edu.umb.cs680.hw05;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+	private String[] carToStringArray(Car car) {
+		String[] carInfo = {car.getMake(),
+							car.getModel(),
+							Integer.toString(car.getMileage()),
+							Integer.toString(car.getYear()),
+							Float.toString(car.getPrice())};
+		return carInfo;
+	}
+	
+	@Test
+	public void verifyCarEqualityWithMakeModelYear() {
+		try {
+			int mileage = 100;
+			float price = 10000L;
+			String[] expected = {"Toyota", "RAV4", "100", "2018", "10000"};
+			Car actual = new Car("Toyota", "RAV4", mileage, 2018, price);
+			assertArrayEquals(expected, carToStringArray(actual));
+		}
+		catch(RuntimeException e) {
+			fail();
+		}
+	}
+}
